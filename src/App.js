457012f702ba22091b3f67bdde6a1a15720815f9
Hiren Khanchandani <hiren14@Hirens-MacBook-Air.local>
2024-04-20 17:10:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Chart from './components/Chart';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
